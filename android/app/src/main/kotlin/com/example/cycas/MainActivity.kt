648ca628aa4c44234a8ad66977c840db12101d85
ayat <ayataliev0@gmail.com>
2021-09-20 20:47:39 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/cycas/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cycas
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:cycas/presentation/sellers/main/main_sellers.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      statusBarIconBrightness: Brightness.dark,
+      statusBarColor: Colors.white,
+    ));
+
+    return const MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      home: MainSellers(),
+    );
+  }
+}
